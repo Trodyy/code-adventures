652be54d59c9bddc90d69ca15005bb5b698b8f90
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-10-30 01:37:58 +0330

    DOCS : Modify var names and add comments.

--- biggest-sector-between-matrices/code.py
+++ biggest-sector-between-matrices/code.py
@@ -1,19 +1,21 @@
-import random
+import random    #use this library to create matrices with 100x100 size.
 
 
 
+####create 10 matrices with 1000x1000 size with random values between 0 to 255.
 
-list = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j']
-dicts = {}
-for shirin in list : 
+#create a list of 10 names for matrices.
+list_of_matrices_names = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j']     
+all_matrices = {}      #Initilize dict of all matrices.
+for matrice_name in list_of_matrices_names :  #Loop through each name in the list.
     lists = [[] , [] , [] , [] , [] ,[] , [] ,[] , [] , []]
-    for kh in lists :
-        for i in range(1000) :
+    for matrice_values in lists :    #Loop through each list in the lists.
+        for i in range(100) :     #Create 100 rows for each matrix.
             b = []
-            for j in range(1000) :
-                b.append(random.randint(0,255))
-            kh.append(b)
-    dicts[shirin] = kh
+            for j in range(100) :    #Create 100 columns for each matrix.
+                b.append(random.randint(0,255))      #Create a list with 100 random values between 0 to 255.
+            matrice_values.append(b)
+    all_matrices[matrice_name] = matrice_values        #Add each matrix to the dict with its name as key.
 
 
 
@@ -21,11 +23,11 @@ buffer_list = []
 
 buffer = [[],[],[],[],[],[],[],[],[]]
 
-for item in list[1:] :
-    for i in range(len(dicts['a'])) :
-        for j in range(len(dicts['a'][0])) :
-            if dicts['a'][i][j] == dicts[item][i][j] :
-                buffer[list.index(item) - 1].append(dicts['a'][i][j])
+for item in list_of_matrices_names[1:] :
+    for i in range(len(all_matrices['a'])) :
+        for j in range(len(all_matrices['a'][0])) :
+            if all_matrices['a'][i][j] == all_matrices[item][i][j] :
+                buffer[list_of_matrices_names.index(item) - 1].append(all_matrices['a'][i][j])
 
 
 
@@ -42,6 +44,6 @@ for j in buffer :
 
 result = {}
 for i in buffer2 : 
-    result[list[buffer2.index(i) + 1]] = sum(i.values())
+    result[list_of_matrices_names[buffer2.index(i) + 1]] = sum(i.values())
 print(result)
 
