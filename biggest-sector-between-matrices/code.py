23291ebe0a998cfef928f599ee894334fc45f6fa
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-10-30 16:00:12 +0330

    DOCS : Rename vars and Add comments.

--- biggest-sector-between-matrices/code.py
+++ biggest-sector-between-matrices/code.py
@@ -13,36 +13,40 @@ for matrice_name in list_of_matrices_names :  #Loop through each name in the lis
     for matrice_values in lists :    #Loop through each list in the lists.
         for rows in range(array_size) :     #Create 100 rows for each matrix.
             b = []
-            for columns in range(array_size) :    #Create 100 columns for each matrix.
+            for elements in range(array_size) :    #Create 100 columns for each matrix.
                 b.append(random.randint(0,255))      #Create a list with 100 random values between 0 to 255.
             matrice_values.append(b)
     all_matrices[matrice_name] = matrice_values        #Add each matrix to the dict with its name as key.
 
 
 
-buffer = [[],[],[],[],[],[],[],[],[]]   
+list_of_same_elements = [[],[],[],[],[],[],[],[],[]]   
 
 for item in list_of_matrices_names[1:] :
     for i in range(len(all_matrices['a'])) :
-        for j in range(len(all_matrices['a'][0])) :
-            if all_matrices['a'][i][j] == all_matrices[item][i][j] :
-                buffer[list_of_matrices_names.index(item) - 1].append(all_matrices['a'][i][j])
+        for j in range(len(all_matrices['a'][0])) :     #Loop through each element in matrix 'a'.
+            if all_matrices['a'][i][j] == all_matrices[item][i][j] :#Compare each element in matrix 'a' with the corresponding element in the other matrices.
+
+                #If the elements are the same, add the element to the list of same elements.
+                list_of_same_elements[list_of_matrices_names.index(item) - 1].append(all_matrices['a'][i][j])   
+                 
 
 
 
 
 
-buffer2 = [{} , {} , {} , {} , {} , {} , {} , {} , {}]
+reducer = [{} , {} , {} , {} , {} , {} , {} , {} , {}]
 
-for j in buffer :
+#Reduce the list of same elements to a dict with the count of each element.
+for j in list_of_same_elements :
     for i in j :
-        if f"{i}" in buffer2[buffer.index(j)]:
-            buffer2[buffer.index(j)][f"{i}"] += 1
-        else:
-            buffer2[buffer.index(j)][f"{i}"] = 1
+        if f"{i}" in reducer[list_of_same_elements.index(j)]:   #If the element is already in the dict, increment its count by 1.
+            reducer[list_of_same_elements.index(j)][f"{i}"] += 1   #Increment the count of the element by 1.
+        else:             #If the element is not in the dict, add it with a count of 1.
+            reducer[list_of_same_elements.index(j)][f"{i}"] = 1    #Add the element to the dict with a count of 1.
 
 result = {}
-for i in buffer2 : 
-    result[list_of_matrices_names[buffer2.index(i) + 1]] = sum(i.values())
-print(result)
+for i in reducer :    #Loop through each dict in the reducer list.
+    result[list_of_matrices_names[reducer.index(i) + 1]] = sum(i.values())   #Sum the counts of each element in the dict and add it to the result dict with the matrix name as key.
+print(result)     #Print the result dict.
 
