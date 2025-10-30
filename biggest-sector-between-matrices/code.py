b81267c4927855f122b52ebfeb3463ca61267635
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-10-30 15:54:14 +0330

    FEAT : Get size of matrice from user.

--- biggest-sector-between-matrices/code.py
+++ biggest-sector-between-matrices/code.py
@@ -1,6 +1,7 @@
 import random    #use this library to create matrices with 100x100 size.
 
 
+array_size = int(input("input the size of matrice : "))
 
 ####create 10 matrices with 1000x1000 size with random values between 0 to 255.
 
@@ -10,16 +11,16 @@ all_matrices = {}      #Initilize dict of all matrices.
 for matrice_name in list_of_matrices_names :  #Loop through each name in the list.
     lists = [[] , [] , [] , [] , [] ,[] , [] ,[] , [] , []]
     for matrice_values in lists :    #Loop through each list in the lists.
-        for i in range(100) :     #Create 100 rows for each matrix.
+        for rows in range(array_size) :     #Create 100 rows for each matrix.
             b = []
-            for j in range(100) :    #Create 100 columns for each matrix.
+            for columns in range(array_size) :    #Create 100 columns for each matrix.
                 b.append(random.randint(0,255))      #Create a list with 100 random values between 0 to 255.
             matrice_values.append(b)
     all_matrices[matrice_name] = matrice_values        #Add each matrix to the dict with its name as key.
 
 
 
-buffer = [[],[],[],[],[],[],[],[],[]]
+buffer = [[],[],[],[],[],[],[],[],[]]   
 
 for item in list_of_matrices_names[1:] :
     for i in range(len(all_matrices['a'])) :
