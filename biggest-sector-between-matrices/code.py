18517c15c2d253ffca383ee246ed6ef564c22847
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-10-30 16:15:49 +0330

    DOCS : Modify mis-types pf matrix's name.

--- biggest-sector-between-matrices/code.py
+++ biggest-sector-between-matrices/code.py
@@ -1,22 +1,22 @@
 import random    #use this library to create matrices with 100x100 size.
 
 
-array_size = int(input("input the size of matrice : "))
+array_size = int(input("input the size of matrix : "))
 
 ####create 10 matrices with 1000x1000 size with random values between 0 to 255.
 
 #create a list of 10 names for matrices.
 list_of_matrices_names = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j']     
 all_matrices = {}      #Initilize dict of all matrices.
-for matrice_name in list_of_matrices_names :  #Loop through each name in the list.
+for matrix_name in list_of_matrices_names :  #Loop through each name in the list.
     lists = [[] , [] , [] , [] , [] ,[] , [] ,[] , [] , []]
-    for matrice_values in lists :    #Loop through each list in the lists.
+    for matrix_values in lists :    #Loop through each list in the lists.
         for rows in range(array_size) :     #Create 100 rows for each matrix.
             b = []
             for elements in range(array_size) :    #Create 100 columns for each matrix.
                 b.append(random.randint(0,255))      #Create a list with 100 random values between 0 to 255.
-            matrice_values.append(b)
-    all_matrices[matrice_name] = matrice_values        #Add each matrix to the dict with its name as key.
+            matrix_values.append(b)
+    all_matrices[matrix_name] = matrix_values        #Add each matrix to the dict with its name as key.
 
 
 
